6701661e1280e6cf5c3d9dcd6877689bf8a59217
= <midhunpkbengalam@gmail>
2022-02-20 13:16:16 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require("request")
+
+const foreCast = (lattitude,longitude,callback) =>{
+  const url = "http://api.weatherstack.com/current?access_key=eb3cdb62f78b7ab3ba6f8236905135f3&query="+lattitude+","+longitude+"&units=f"
+  
+ 
+
+request({url,json:true},(error,{body}) =>{
+  if(error){
+    callback("Unable to connect to weather service!",undefined)
+  }
+  else if(body.error){
+    callback("Unable to find location",undefined)
+  }
+  else{
+   callback(undefined, body.current.weather_descriptions[0] + ". it is currently "+ body.current.temperature + " degress out.It feels like " + body.current.feelslike + " degress out")
+  }
+ 
+ 
+  
+})
+}
+module.exports = foreCast
